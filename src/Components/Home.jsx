111011958e8f13957ab0318ab6f1b8b79c8d4e31
aru-shi2 <arushimishra2929@gmail.com>
2025-09-14 11:01:06 -0600

    initial setup

---
+++ src/Components/Home.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const Home = () => {
+  return (
+    <div>
+      <div className="w-full">
+        <img src="public\images\home.jpg" alt="" />
+      </div>
+    </div>
+  )
+}
+
+export default Home
