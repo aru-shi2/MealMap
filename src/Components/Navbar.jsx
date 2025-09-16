935f2c75988501a82dba288bee78f83aa664d8ed
aru-shi2 <arushimishra2929@gmail.com>
2025-09-16 12:09:45 -0600

    Home page completed

---
+++ src/Components/Navbar.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+
+const Navbar = () => {
+  return (
+    <div>
+        <div className="logo">MealMap</div>
+      <div>
+        <span><a href=""></a>Home</span>
+        <span><a href=""></a>Recipe</span>
+      </div>
+    </div>
+  )
+}
+
+export default Navbar
